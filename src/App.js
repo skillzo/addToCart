e1c123ef95720afc72d891bd59d29081ad0bdb9d
Chukwu Emmanuel <skillzfire@yahoo.com>
2022-08-27 17:28:31 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import ProductsPage from "./pages/Products";
+import CartPage from "./pages/Cart";
+import GlobalState from "./context/GlobalState";
+
+function App(props) {
+  return (
+    <GlobalState>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={ProductsPage} exact />
+          <Route path="/cart" component={CartPage} exact />
+        </Switch>
+      </BrowserRouter>
+    </GlobalState>
+  );
+}
+
+export default App;
